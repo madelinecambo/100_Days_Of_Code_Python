f99b3885ed61a6211942baaca4f8ec967ac1894b
Madeline Cambo <madelineann11@gmail.com>
2024-07-12 18:12:56 -0400

    Updated folder structure

---
+++ Day3-Treasure_Island_Game/main.py
@@ -0,0 +1,51 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+choice1 = input("You're at a crossroad you like to take the Left or Right path?").lower()
+
+if choice1 == 'left':
+    #continue the game
+    choice2 = input("Would you like to swim across the channel or wait. Type \"wait\" or \"swim\"?")
+    if choice2 == 'swim':
+        print("You were attacked by a trout. Game Over!")
+    else:
+        door_decision = input("There are three doors. Red, Yellow or Blue. Which do you choose?").lower()
+        if door_decision == 'red':
+            print("You were burned by a fire. Game Over!")
+        elif door_decision == 'blue':
+            print("You were eaten by beasts. Game Over!")
+        elif door_decision == 'yellow':
+            print("You Win! You Found the Treasure")
+        else:
+            print('Wrong Choice! Game Over.')
+else:
+    print("A monster attacked you on the path. Game Over!")
+
+
+
