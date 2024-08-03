5308c03872fb008aec45419f0b59ebe131b88853
Madeline Cambo <madelineann11@gmail.com>
2024-08-03 17:33:10 -0400

    Added code for notes and day 4 project

--- Day21-Snake_Game-Part2/main.py
+++ Day21-Snake_Game-Part2/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 
@@ -10,6 +12,9 @@ screen.title("My Snake Game!")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,11 +28,29 @@ while game_is_on:
     time.sleep(.1)
     snake.move()
 
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.score_point()
+        snake.extend()
 
+    #Detech collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
+    #Detect collision with tail.
+    #if head collides with any segment in the tail:
+        #trigger game over sequence
 
-
-
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
+
+
+
+
