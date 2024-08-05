faad01eddd60cb47505ab29328708b076091ff19
Madeline Cambo <madelineann11@gmail.com>
2024-08-05 15:39:10 -0400

    Added code for notes and day 4 project

--- Day24-Mail_Merge_Project/main.py
+++ Day24-Mail_Merge_Project/main.py
@@ -1,7 +1,7 @@
 import re
+PLACEHOLDER = "[name]"
 
 # Create a letter using starting_letter.txt
-
 
 # Read in the names
 with open("./Input/Names/invited_names.txt") as names:
@@ -11,18 +11,10 @@ print(name_list)
 
 # Read in the Starting Letter
 with open("./Input/Letters/starting_letter.txt") as letter:
-    letter_list = letter.readlines()
-
-
-
-
-# Loop through and replace name in Each Starting Letter
-for name in name_list:
-    first_line = [f"{letter_list[0].replace("[name]", name)}"]
-    complete_letter = first_line + letter_list[1:]
-
-# Write the New letter to the ReadyToSend Directory
-    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as invite:
-        for line in complete_letter:
-            invite.write(line)
+    letter_contents = letter.read()
+    for name in name_list:
+        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        # Write the New letter to the ReadyToSend Directory
+        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as completed_letter:
+                completed_letter.write(new_letter)
 
