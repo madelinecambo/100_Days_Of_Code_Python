7da8742d02d708a3ab7222ed002af0d586b80e5d
Madeline Cambo <madelineann11@gmail.com>
2024-08-13 18:07:56 -0400

    Added code for notes and day 4 project

--- Day32-Birthday_Email_Sender/main.py
+++ Day32-Birthday_Email_Sender/main.py
@@ -8,11 +8,9 @@ import os
 MY_EMAIL = ""
 MY_PASSWORD = ""
 
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-today_day = dt.datetime.now().day
-today_month = dt.datetime.now().month
+today = dt.datetime.now()
+today_day = today.day
+today_month = today.month
 
 letters_list = os.listdir("letter_templates")
 
@@ -35,31 +33,12 @@ if len(today_birthdays) > 0:
         email_message = f"Subject:{email_subject}\n\n{birthday_message}"
 
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-            # makes connection secure
             connection.starttls()
             connection.login(user=MY_EMAIL, password=MY_PASSWORD)
             connection.sendmail(from_addr=MY_EMAIL,
                                 to_addrs=birthday_email,
                                 msg=email_message)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
 
 
 
