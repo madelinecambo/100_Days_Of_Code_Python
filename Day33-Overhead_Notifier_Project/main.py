ed714f832831e288da4b2b4631ace176f20a1b54
Madeline Cambo <madelineann11@gmail.com>
2024-08-14 11:25:47 -0400

    Added code for notes and day 4 project

--- Day33-Overhead_Notifier_Project/main.py
+++ Day33-Overhead_Notifier_Project/main.py
@@ -3,15 +3,15 @@ from datetime import datetime
 import time
 import smtplib
 
-MY_LAT = 35.791538
-MY_LONG = -78.781120
+MY_LAT = 40.730610
+MY_LONG = -73.935242
 MY_EMAIL = ""
 MY_PASSWORD = ""
 TO_EMAIL = ""
 
 def send_email():
-    email_subject = f"ISS is Overhead!"
-    email_message = f"Subject:{email_subject}\n\nISS is Overhead, Look up!"
+    email_subject = f"Look up☝️"
+    email_message = f"Subject:{email_subject}\n\nThe ISS is above you in the sky!"
 
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
@@ -29,11 +29,10 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 # Function for: Your position is within +5 or -5 degrees of the ISS position.
-def iss_overhead_current_location():
-    if (iss_latitude - MY_LAT <= 5) & (iss_longitude - MY_LONG <= 5):
+def is_iss_overhead():
+    if (abs(iss_latitude - MY_LAT) <= 5) and (abs(iss_longitude - MY_LONG) <= 5):
         return True
-    else:
-        return False
+
 
 parameters = {
     "lat": MY_LAT,
@@ -51,17 +50,18 @@ time_now = datetime.now()
 #If the ISS is close to my current position
 # and it is currently dark
 
-def during_the_night():
-    if (time_now.hour > sunset) & (time_now.hour < sunrise):
+def is_night():
+    if (time_now.hour >= sunset) or (time_now.hour <= sunrise):
         return True
-    else:
-        return False
+
 
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
-while (during_the_night() == True) and (iss_overhead_current_location() == True):
+
+while True:
     time.sleep(60)
-    send_email()
+    if is_night() and is_iss_overhead():
+        send_email()
 
 
 
