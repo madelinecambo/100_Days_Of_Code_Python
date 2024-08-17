0c6a94f85ab8b148d9716545f2fe509f694880ed
Madeline Cambo <madelineann11@gmail.com>
2024-08-17 15:57:24 -0400

    Added code for notes and day 4 project

--- Day35-Weather_Email_App/main.py
+++ Day35-Weather_Email_App/main.py
@@ -3,6 +3,7 @@ import requests
 import datetime as dt
 from dotenv import load_dotenv
 import os
+import smtplib
 
 load_dotenv("C:/Users/madel/OneDrive/Projects/PythonEnvironmentVariables/.env.txt")
 
@@ -12,6 +13,11 @@ LAT = 35.867512
 LON = -78.606628
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 RAIN_ID = 700
+
+# Email Related Constants
+MY_EMAIL = os.getenv("MyEmail2")
+MY_PASSWORD = os.getenv("Email2AppPassword")
+TO_EMAIL = os.getenv("MyEmail1")
 
 parameters = {
     "lat": LAT,
@@ -32,8 +38,24 @@ for hour_data in forecasts_json['list']:
     if int(condition_code) < RAIN_ID:
         will_rain = True
 
+def send_email():
+    email_subject = f"Today's Forecast Has Rain"
+    email_message = f"Subject:{email_subject}\n\nBring an Umbrella!"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=TO_EMAIL,
+                            msg=email_message)
+    print("Email Sent!")
+
 if will_rain:
     print("Bring an Umbrella!")
+    send_email()
+
+
+
 
 
 
