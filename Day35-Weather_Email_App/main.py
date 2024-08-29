0133fb927d20cd21c99ef959ff18b65478a31683
Madeline Cambo <madelineann11@gmail.com>
2024-08-29 12:19:16 -0400

    Added code for notes and day 4 project

--- Day35-Weather_Email_App/main.py
+++ Day35-Weather_Email_App/main.py
@@ -4,7 +4,7 @@ import datetime as dt
 from dotenv import load_dotenv
 import os
 import smtplib
-from weather import check_rain, check_cold_rule, check_heat_rule
+from weather import check_rain, check_cold_rule, check_heat_rule, format_weather_email
 
 load_dotenv("C:/Users/madel/OneDrive/Projects/PythonEnvironmentVariables/.env.txt")
 
@@ -24,19 +24,22 @@ TO_EMAIL = os.getenv("MyEmail1")
 
 # Date Constants
 TODAY = dt.datetime.now().date()
-MATCH_START_TIME = pd.to_datetime(str(TODAY) + ' ' + "18:00")
-COMBO_MATCH_DATES = ['2024-08-17', '2024-08-19', '2024-08-26', '2024-09-02', '2024-09-09', '2024-09-16', '2024-09-23']
-COMBO_MATCH_DATES = [pd.to_datetime(date) for date in COMBO_MATCH_DATES]
-MIXED_MATCH_DATES = pd.date_range('2024-09-14', '2024-11-14', freq='7d').tolist()
+MATCH_START_TIME = pd.to_datetime(str(TODAY) + ' ' + "18:00:00").strftime('%H:%M:%S')
+TOURNAMENT_DATES = ['2024-08-29','2024-08-31', '2024-09-01', '2024-09-02']
+COMBO_MATCH_DATES = ['2024-08-17', '2024-08-19', '2024-08-20', '2024-08-26', '2024-09-02', '2024-09-09', '2024-09-16', '2024-09-23']
+MIXED_MATCH_DATES = ['2024-09-13', '2024-09-21', '2024-09-28', '2024-10-05', '2024-10-26', '2024-11-01', '2024-11-09', '2024-11-23']
 
-if pd.to_datetime(TODAY) in COMBO_MATCH_DATES or TODAY in MIXED_MATCH_DATES:
+ALL_DATES = TOURNAMENT_DATES + COMBO_MATCH_DATES + MIXED_MATCH_DATES
+ALL_DATES = [pd.to_datetime(date).date() for date in ALL_DATES]
+
+if TODAY in ALL_DATES:
     print("There is a match today!")
 
     parameters = {
         "lat": LAT,
         "lon": LON,
         "appid": API_KEY,
-        "cnt": 4,
+        "cnt": 3,
         "units": 'imperial'
     }
     response = requests.get(url=OWM_ENDPOINT, params=parameters)
@@ -49,40 +52,38 @@ if pd.to_datetime(TODAY) in COMBO_MATCH_DATES or TODAY in MIXED_MATCH_DATES:
 
     for idx, forecast in enumerate(forecast_3hr):
         weather_ids.append(forecast['weather'][0]['id'])
-        date_time = forecast['dt_txt']
-        if pd.to_datetime(date_time) == MATCH_START_TIME:
-            forecast_index = idx
-        else:
-            forecast_index = 0
+        print(forecast)
+        if pd.to_datetime(forecast['dt_txt']).strftime('%H:%M:%S') == MATCH_START_TIME:
+            humidity = forecast['main']['humidity']
+            weather_main = forecast['weather'][0]['main']
+            weather_description = forecast['weather'][0]['description']
+            wind_mph = forecast['wind']['speed']
+            starting_temp = forecast['main']['feels_like']
 
-    starting_temp = forecast_3hr[forecast_index]['main']['feels_like']
 
-    sms_message = [x for x in [check_rain(weather_ids, RAIN_ID),
+    status_message_list = [x for x in [check_rain(weather_ids, RAIN_ID),
                                check_heat_rule(starting_temp, HIGH_TEMP_FEELS_LIKE),
                                check_cold_rule(starting_temp, LOW_TEMP_FEELS_LIKE)] if x is not None]
 
-    if sms_message == 0:
-        sms_message = f"The weather conditions are favorable for tonight's matches!\nThe temperature will be {starting_temp} degrees. Have fun!"
-        status = f"OK - (Temperature {starting_temp}F)"
+    if len(status_message_list) == 0:
+        status = f"OK: Temperature {round(starting_temp)}F/Humidity {humidity}%)"
         print(status)
-    elif len(sms_message) == 1:
-        sms_message.append("Matches may need to be rescheduled!")
-        sms_message = "\n".join(sms_message)
-        status = f"Potential Reschedule (Temperature {starting_temp}F)"
+    elif len(status_message_list) == 1:
+        status = f"{status_message_list[0]} (Potential Reschedule)"
         print(status)
-    elif len(sms_message) > 1:
-        sms_message.append("Matches may need to be rescheduled!")
-        sms_message = "\n".join(sms_message)
-        status = f"Potential Reschedule (Temperature {starting_temp}F)"
+    elif len(status_message_list) > 1:
+        status_message = " & ".join(status_message_list)
+        status = f"{status_message} (Potential Reschedule)"
         print(status)
     else:
-        sms_message = f"The weather conditions are favorable for tonight's matches!\nThe temperature will be {starting_temp}F. Have fun!"
-        status = "OK - Temperature {starting_temp}F"
+        status = f"OK: Temperature {round(starting_temp)}F/Humidity {humidity}%)"
 
 
-    def send_email(sms_message):
-        email_subject = f"Tennis Match Status for Tonight: {status}"
-        email_message = f"Subject:{email_subject}\n\n{sms_message}"
+    subject, body = format_weather_email(status, starting_temp, humidity, wind_mph, weather_main, weather_description)
+
+
+    def send_email(subject, body):
+        email_message = f"Subject:{subject}\n\n{body}"
 
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
@@ -92,7 +93,7 @@ if pd.to_datetime(TODAY) in COMBO_MATCH_DATES or TODAY in MIXED_MATCH_DATES:
                                 msg=email_message)
         print("Email Sent!")
 
-    send_email(sms_message)
+    send_email(subject, body)
 
 
 
--- Day35-Weather_Email_App/weather.py
+++ Day35-Weather_Email_App/weather.py
@@ -3,16 +3,35 @@
 
 def check_rain(weather_ids, rain_id):
    if (any(x < rain_id for x in weather_ids)):
-       return("There is rain in the forecast for today.")
+       return("Forecasted Rain")
 
 
 def check_heat_rule(starting_temp, USTA_heat):
     if starting_temp >= USTA_heat:
-        return(f"The temperature is forecasted to be {starting_temp} at matchtime. USTA heat rule is exceeded.")
+        return(f"USTA Heat Rule Warning")
 
 
 def check_cold_rule(starting_temp, USTA_cold):
     if starting_temp < USTA_cold:
-        return(f"The temperature is forecasted to be {starting_temp} at matchtime. USTA cold rule is exceeded.")
+        return(f"USTA Cold Rule Warning")
 
+
+def format_weather_email(status, temperature, humidity, wind_speed, weather_main, weather_description):
+    subject = f"Tennis Match Weather Alert: {status}"
+    body = f"""
+
+    Please find below the weather forecast for today's match.
+
+    Weather Forecast for 6pm:
+    - Temperature: {round(temperature)}F
+    - Humidity: {humidity}%
+    - Wind Speed: {round(wind_speed)} mph
+    - General Weather: {weather_main} - {weather_description}
+
+    Please take this forecast into consideration as you prepare for the match. Should the weather conditions be unfavorable, you may want to consider rescheduling the match.
+
+    Good luck!
+    """
+
+    return subject, body
 
